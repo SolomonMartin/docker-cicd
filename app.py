77a6eaa6db58a71686f58a68e177d11d153baf69
SolomonMartin <you@example.com>
2024-09-08 18:25:44 -0400

    Docker ci cd yaml is ready

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-## This project shows how to work with github actions
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=5000) 
